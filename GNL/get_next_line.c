086dcae3b769f8deea0d02f57f7610f107a5692a
Francesco LEE <flee@e1r4p4.42.fr>
2021-06-28 16:44:43 +0200

    test

--- GNL/get_next_line.c
+++ GNL/get_next_line.c
@@ -6,13 +6,26 @@
 /*   By: flee <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/27 10:49:31 by flee              #+#    #+#             */
-/*   Updated: 2021/06/27 11:21:31 by flee             ###   ########.fr       */
+/*   Updated: 2021/06/28 16:39:44 by flee             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-
+#include "get_next_line.h"
 
 int	get_next_line(int fd, char **line)
 {
-	ssize_t read(int fd, void *buf, size_t count);
+	static	char *memory;
+	int count_byte;
+	char *tmp[BUFFER_SIZE];
+
+	count_byte = 0;
+	if (fd)
+	{
+		while (tmp[i] != '\n' || tmp[i] != EOF)
+		{
+		}
+	}
+	return (NULL);
+	
+
 }
