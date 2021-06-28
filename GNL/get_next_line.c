17daa11a64da52cf27c2a6e21bff8b1e99245fcf
cesco <cesco@kali.cesco.com>
2021-06-28 20:26:25 +0200

    test

---
+++ GNL/get_next_line.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: flee <marvin@42.fr>                        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/06/27 10:49:31 by flee              #+#    #+#             */
+/*   Updated: 2021/06/28 16:39:44 by flee             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+int	get_next_line(int fd, char **line)
+{
+	static	char *memory;
+	int count_byte;
+	char *tmp[BUFFER_SIZE];
+
+	count_byte = 0;
+	if (fd)
+	{
+		while (tmp[i] != '\n' || tmp[i] != EOF)
+		{
+		}
+	}
+	return (NULL);
+	
+
+}
